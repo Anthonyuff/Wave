3ead1c8b89b8467b7cc5708e307ef6847275cb47
Anthonyuff <anthonyrebello612@gmail.com>
2026-04-06 16:07:23 -0300

    add plot

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from .cfg import Config
 from .model import Model
 from .propa import Wave1D
+from .propa import Wave2D
 
 
 
 
 
 
-__all__ = ["Config", "Model","Wave1D"]
+__all__ = ["Config", "Model","Wave1D","Wave2D"]
